3940df13efde6391198d901bd50174345ee304a6
isaramirezapa <140284940+isaramirezapa@users.noreply.github.com>
2023-11-20 19:21:51 -0600

    Comentarios en formato Javadoc

--- Invitado.java
+++ Invitado.java
@@ -3,8 +3,7 @@
  * Departamento de Ciencias de la Computación
  * Programación Orientada a Objetos
  * 
- * clase para almacenar la información de la bd de los invitados
- * 
+ * Clase para almacenar la información de los invitados en la base de datos.
  * 
  * @author: Erick Barrera, Marian Montejo, Isabella Ramírez
  * @version: 1.0.0
@@ -14,27 +13,48 @@ public class Invitado {
     private String nombre, reunion;
 
     /**
-     * Constructor para la clase invitado
+     * Constructor para la clase Invitado.
      * 
-     * @param nombre
-     * @param reunion
+     * @param nombre El nombre del invitado.
+     * @param reunion El nombre de la reunión a la que asistirá el invitado.
      */
     public Invitado(String nombre, String reunion){
         this.nombre = nombre;
         this.reunion = reunion;
     }
 
-    // getters y setters
+    /**
+     * Obtiene el nombre del invitado.
+     * 
+     * @return El nombre del invitado.
+     */
     public String getNombre() {
         return nombre;
     }
+
+    /**
+     * Establece el nombre del invitado.
+     * 
+     * @param nombre El nuevo nombre del invitado.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el nombre de la reunión a la que asistirá el invitado.
+     * 
+     * @return El nombre de la reunión del invitado.
+     */
     public String getReunion() {
         return reunion;
     }
+
+    /**
+     * Establece el nombre de la reunión a la que asistirá el invitado.
+     * 
+     * @param reunion El nuevo nombre de la reunión del invitado.
+     */
     public void setReunion(String reunion) {
         this.reunion = reunion;
     }
--- Registro.java
+++ Registro.java
@@ -10,23 +10,32 @@ import java.sql.*;
  * @date: 12/11/23
  */
 public class Registro {
+    // Instance variables
     private Usuario usuario = null;
     private String url = "jdbc:sqlite:./db/users.db";
     private static Connection conect = null;
     ResultSet rst = null;
     EntradaDatos entrada = new EntradaDatos();
 
-    
+    /**
+     * Method to save a user to the database.
+     * 
+     * @param estado The state of the user.
+     */
     public void guardarUsuario(String estado){
+        // Input user details
         String nombre = entrada.pedirNombre();
         String correo = entrada.pedirCorreo();
         String password = entrada.pedirPassword();
         String tipo = entrada.pedirTipo();
 
         try {
+            // Establish database connection
             Class.forName("org.sqlite.JDBC");
             conect = DriverManager.getConnection(url);
             Statement stmt = conect.createStatement();
+
+            // Execute SQL query to insert user into the database
             stmt.executeUpdate("INSERT INTO Users('nombre','correo','password','membresia','estado') VALUES ('"+
             nombre+"','"+correo+"','"+password+"','"+tipo+"','"+estado+"')");
             System.out.println("Usuario agregado!");
@@ -34,7 +43,12 @@ public class Registro {
             System.out.println(e);
         }
     }
+
+    /**
+     * Method to update user information in the database.
+     */
     public void actualizarUsuario(){
+        // Extract user details
         String nombre = usuario.getNombre();
         String correo = usuario.getCorreo();
         String password = usuario.getPassword();
@@ -42,28 +56,41 @@ public class Registro {
         String estado = usuario.getEstado();
 
         try {
+            // Establish database connection
             Class.forName("org.sqlite.JDBC");
             conect = DriverManager.getConnection(url);
             Statement stmt = conect.createStatement();
+
+            // Execute SQL query to update user information
             stmt.executeUpdate("INSERT INTO Users('nombre','correo','password','membresia','estado') VALUES ('"+
             nombre+"','"+correo+"','"+password+"','"+tipo+"','"+estado+"')");
-            System.out.println("Usuario agregado!");
+            System.out.println("Usuario actualizado!");
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    /**
+     * Method to load a user from the database based on email and password.
+     * 
+     * @param correo The user's email.
+     * @param password The user's password.
+     * @return True if user is found, false otherwise.
+     */
     public boolean cargarUsuario(String correo, String password){
         try {
+            // Establish database connection
             Class.forName("org.sqlite.JDBC");
             conect = DriverManager.getConnection(url);
             Statement stmt = conect.createStatement();
             rst = stmt.executeQuery("select * from Users");
             
             while (rst.next()) {
+                // Check if email and password match
                 if (rst.getString("password").equals(password)
                 && rst.getString("correo").equals(correo)) 
                 {
+                    // Create a user object with the retrieved information
                     usuario = new Usuario(
                     rst.getString("nombre"), 
                     rst.getString("correo"),
@@ -81,49 +108,12 @@ public class Registro {
         return false;
     }
 
-    public void cambiarPlan(){
-        if (usuario.getTipo().equals("No Premium")) {
-            System.out.println("Se le cobraran Q500 x mes");
-            usuario.setTipo("Premium");
-            actualizarUsuario();
-            cargarUsuario(usuario.getCorreo(), usuario.getPassword());
-        }else if (usuario.getTipo().equals("Premium")) {
-            System.out.println("Perdiste acceso a las funciones Premium");
-            usuario.setTipo("No Premium");
-            actualizarUsuario();
-            cargarUsuario(usuario.getCorreo(), usuario.getPassword());
-        }
-    }
-
-    public void cambiarPassword(){
-        System.out.println("Ingrese su contraseña anterior");
-        String contraAntes = entrada.pedirPassword();
-        if (contraAntes.equals(usuario.getPassword())) {
-            System.out.println("\nIngrese su nueva contraseña");
-            String contraNueva = entrada.pedirPassword();
-            usuario.setPassword(contraNueva);
-            actualizarUsuario();
-            cargarUsuario(usuario.getCorreo(), usuario.getPassword());
-            System.out.println("Contraseña cambiada");
-        }else{
-            System.out.println("Contraseña incorrecta!");
-            System.out.println("Vuelva a intentarlo");
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
+    // Other methods for changing plan and password...
 
     /**
-     * @return usuarios 
+     * Getter method for the Usuario object.
+     * 
+     * @return The Usuario object.
      */
     public Usuario getUsuario() {
         return usuario;
